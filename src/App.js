bb611e8b85b287c01f0229fb16961a8af2e6ad68
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-12-29 11:25:35 -0300

    EQ-10: create who we are component with basic styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 import Header from "./Components/Header";
+import WhoWeAre from "./Components/WhoWeAre";
 
 function App() {
   return (
     <main>
       <Header />
+      <WhoWeAre />
     </main>
   );
 }
---
+++ src/Components/WhoWeAre/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./who.module.css";
+
+const WhoWeAre = () => {
+  return (
+    <section className={styles.background}>
+      <div className={styles.presentation}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem
+          quam, semper ac arcu non, sodales pellentesque libero. Sed mattis,
+          velit ac sagittis cursus, tortor ante porttitor purus, ac rhoncus eros
+          magna sed nulla. Orci varius natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus. Nam tincidunt nunc non enim
+          ruaccumsan magna.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default WhoWeAre;
