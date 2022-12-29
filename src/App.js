ceff59fa054404e0665d5c13095ec0c21bb004d7
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-12-29 11:41:48 -0300

    EQ-11: create footer with simple css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 
 import Header from "./Components/Header";
 import WhoWeAre from "./Components/WhoWeAre";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <main>
       <Header />
       <WhoWeAre />
+      <Footer />
     </main>
   );
 }
---
+++ src/Components/Footer/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+import React from "react";
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footerContainer}>
+      <div>
+        <img
+          className={styles.socialMedia}
+          src={`${process.env.PUBLIC_URL}/images/footer/instagram.png`}
+          alt="instagram"
+        />
+        <img
+          className={styles.socialMedia}
+          src={`${process.env.PUBLIC_URL}/images/footer/linkedin.png`}
+          alt="linkedin"
+        />
+      </div>
+      <p>Copyright © 2022 Equilibr.ar. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
