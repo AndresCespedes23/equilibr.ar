d39deb6bddd0cfc117fbc102d3bba55ab53eef87
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-01-01 14:25:39 -0300

    EQ-17: fix css problem with awesome carousel

--- src/Components/Clients/index.js
+++ src/Components/Clients/index.js
@@ -8,11 +8,13 @@ const Clients = () => {
   return (
     <section className={styles.clientsBackground}>
       <h3 className={styles.title}>Clientes</h3>
-      <AwesomeSlider className={styles.awsCarousel}>
-        <div data-src={`${process.env.PUBLIC_URL}/images/background/1.jpg`} />
-        <div data-src={`${process.env.PUBLIC_URL}/images/background/2.jpg`} />
-        <div data-src={`${process.env.PUBLIC_URL}/images/background/4.jpg`} />
-      </AwesomeSlider>
+      <div className={styles.awsCarousel}>
+        <AwesomeSlider>
+          <div data-src={`${process.env.PUBLIC_URL}/images/background/1.jpg`} />
+          <div data-src={`${process.env.PUBLIC_URL}/images/background/2.jpg`} />
+          <div data-src={`${process.env.PUBLIC_URL}/images/background/4.jpg`} />
+        </AwesomeSlider>
+      </div>
       <video width="320" height="240" autoPlay muted>
         Your browser does not support the video tag.
       </video>
