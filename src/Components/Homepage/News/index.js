a7e706c85ca5ac91c46afb957a11ea5587723148
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-01-20 18:24:45 -0300

    EQ-31: add blog page to the news component

--- src/Components/Homepage/News/index.js
+++ src/Components/Homepage/News/index.js
@@ -2,11 +2,17 @@
 import React from "react";
 import styles from "./news.module.css";
 import "react-awesome-slider/dist/styles.css";
+import { Link } from "react-router-dom";
 
 const News = () => {
   return (
     <section id="novedades" className={styles.blogBackground}>
       <h3 className={styles.title}>Novedades</h3>
+      <button className={styles.newsBtn}>
+        <Link className={styles.linkBtn} to="/blog">
+          Ver Novedades
+        </Link>
+      </button>
     </section>
   );
 };
