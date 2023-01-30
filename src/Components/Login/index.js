5e7445b6658ccfd99b986063a8d84b2d7988dcd7
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-01-30 18:08:16 -0300

    EQ-31: add basic login functionality

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../Helpers/Firebase";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/admin");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        navigate("/");
+      });
+  };
   return (
     <div>
-      <label id="email">Email</label>
-      <input id="email" placeholder="email" />
-      <label id="contraseña">Contraseña</label>
-      <input id="contraseña" placeholder="contraseña" />
+      <form>
+        <div>
+          <label id="email">Email</label>
+          <input
+            id="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label id="contraseña">Contraseña</label>
+          <input
+            id="contraseña"
+            placeholder="contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" onClick={onSubmit}>
+          Ingresar
+        </button>
+      </form>
     </div>
   );
 };
--- src/Helpers/Firebase/index.js
+++ src/Helpers/Firebase/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +21,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// eslint-disable-next-line no-unused-vars
 const analytics = getAnalytics(app);
+export const auth = getAuth();
